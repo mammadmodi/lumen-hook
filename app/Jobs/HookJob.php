07607472fcdca0fabe0494f8cc1ab06e341c82b7
mohammadreza.modares <mohammadreza.modares@snapp.cab>
2021-03-07 01:06:10 +0330

    Add HookJob to application

---
+++ app/Jobs/HookJob.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Hook;
+use App\HookError;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+class HookJob extends Job
+{
+    /**
+     * @var Hook $hook
+     */
+    private $hook;
+
+    /**
+     * @var Client
+     */
+    private $httpClient;
+
+    /**
+     * @var integer
+     */
+    private $retryDelay;
+
+    /**
+     * HookJob constructor.
+     * @param Hook $hook
+     */
+    public function __construct(Hook $hook)
+    {
+        $this->hook = $hook;
+        $this->retryDelay = config("hook.retryDelay");
+        $this->httpClient = new Client([
+            'timeout' => config("hook.timeout"),
+        ]);
+    }
+
+    /**
+     * Handle tries to perform a hook until gets a success response.
+     */
+    public function handle()
+    {
+        for ($i = 1; $i <= $this->hook->threshold; $i++) {
+            try {
+                $response = $this->httpClient->post($this->hook->url);
+            } catch (GuzzleException $exception) {
+                if ($i == $this->hook->threshold) {
+                    $this->reportHookError($exception->getCode(), $exception->getMessage());
+
+                    return;
+                }
+                sleep($this->retryDelay);
+                continue;
+            }
+
+            if ($response->getStatusCode() >= 200 || $response->getStatusCode() <= 299) {
+                return;
+            }
+
+            if ($i == $this->hook->threshold) {
+                $this->reportHookError($response->getStatusCode(), $response->getBody());
+
+                return;
+            }
+
+            sleep($this->retryDelay);
+        }
+    }
+
+    /**
+     * Saves a hook error for this hook.
+     *
+     * @param $statusCode
+     * @param $responseBody
+     */
+    private function reportHookError($statusCode, $responseBody)
+    {
+        $hookError = new HookError();
+        $hookError->hook_id = $this->hook->id;
+        $hookError->status_code = $statusCode;
+        $hookError->response_body = $responseBody;
+        $hookError->save();
+    }
+}
--- config/hook.php
+++ config/hook.php
@@ -3,4 +3,7 @@
 return [
     // Hook http timeout in seconds.
     'timeout' => env('HOOK_TIMEOUT', 10),
+
+    // Number of seconds that will be wait to retry hook.
+    'retry_delay' => env('HOOK_RETRY_DELAY', 2),
 ];
