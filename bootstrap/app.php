344966830b745e645a6d0e1b5a5a86379dca00df
mohammadreza.modares <mohammadreza.modares@snapp.cab>
2021-03-07 00:06:38 +0330

    Add http hook configurations

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -63,6 +63,7 @@ $app->singleton(
 
 $app->configure('app');
 $app->configure('jwt');
+$app->configure('hook');
 
 /*
 |--------------------------------------------------------------------------
---
+++ config/hook.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    // Hook http timeout in seconds.
+    'timeout' => env('HOOK_TIMEOUT', 10),
+];
